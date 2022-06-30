ae04210f0882e7abbecb82aa154e5563f7c9d251
Douglas Freitas <freitasd345@gmail.com>
2022-06-30 12:13:57 -0300

    Adicionando repositorio

---
+++ Documents/Udemy/C# Iniciante/01 - Seção/Solucao/01_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Console
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+}
